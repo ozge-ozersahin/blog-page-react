874b176d7128f43769534c5acc6fc213e7ccfa46
Ozge Ozersahin <ozge.ozersahin@skybettingandgaming.com>
2023-11-24 12:32:31 +0000

    added navitem and category slider

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Category } from "../../components/category/Category.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <Category />
+    </>
+  );
+};
+
+export default Home;
