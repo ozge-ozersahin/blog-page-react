43402d664275194beb80c4c633c8ec262d66cd82
Ozge Ozersahin <ozge.ozersahin@skybettingandgaming.com>
2024-01-17 15:28:33 +0000

    card details and icons added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Card } from "../../components/blog/Card.jsx";
 import { Category } from "../../components/category/Category.jsx";
 
 const Home = () => {
   return (
     <>
       <Category />
+      <Card />
     </>
   );
 };
